6ccd47cdff693da8cd5b9f6ad70872ef67f10601
Hank Shen <swh@admpub.com>
2021-11-19 22:32:50 +0800

    update

--- app/src/main/java/com/weihuagu/receiptnotice/util/encrypt/MD5.java
+++ app/src/main/java/com/weihuagu/receiptnotice/util/encrypt/MD5.java
@@ -21,9 +21,9 @@ public class MD5 extends Encrypter {
             Map<String, String> postmap = new HashMap<String, String>();
             postmap.putAll(params);
             postmap.put("sign", getSignMd5WithSecretkey(params.get("type"), params.get("money"), key));
-            LogUtil.debugLogWithJava("调试，sign md5");
-            LogUtil.debugLogWithJava("加密后的map");
-            LogUtil.debugLogWithJava(postmap.toString());
+//            LogUtil.debugLogWithJava("调试，sign md5");
+//            LogUtil.debugLogWithJava("加密后的map");
+//            LogUtil.debugLogWithJava(postmap.toString());
             return postmap;
         }else
         return params;
