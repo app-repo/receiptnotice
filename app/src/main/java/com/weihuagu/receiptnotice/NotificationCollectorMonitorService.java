974000c12f25dde9a99b101fe7087a1343fb25ba
Hank Shen <swh@admpub.com>
2023-07-31 12:10:51 +0800

    update

--- app/src/main/java/com/weihuagu/receiptnotice/NotificationCollectorMonitorService.java
+++ app/src/main/java/com/weihuagu/receiptnotice/NotificationCollectorMonitorService.java
@@ -281,6 +281,7 @@ public class NotificationCollectorMonitorService extends Service {
                 List<ActivityManager.RunningServiceInfo> runningServices = manager.getRunningServices(Integer.MAX_VALUE);
                 if (runningServices == null ) {
                         Log.w(TAG, "ensureCollectorRunning() runningServices is NULL");
+                        toggleNotificationListenerService();
                         return;
                 }
                 for (ActivityManager.RunningServiceInfo service : runningServices) {
@@ -306,7 +307,6 @@ public class NotificationCollectorMonitorService extends Service {
                 PackageManager pm = getPackageManager();
                 pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
                 pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-
         }
 
         @Override
